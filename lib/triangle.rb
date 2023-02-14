7376c3fcc99c14f72dfc8c77f13d31c369d04fb1
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-14 23:02:13 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
-end
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+  @a = a
+  @b = b
+  @c = c
+  end
+  
+  def kind
+  raise TriangleError if invalid?
+  return :equilateral if a == b && b == c
+  return :isosceles if a == b || b == c || a == c
+  :scalene
+  end
+  
+  private
+  
+  def invalid?
+  [a, b, c].min <= 0 || (a + b <= c) || (a + c <= b) || (b + c <= a)
+  end
+  
+   class TriangleError < StandardError; end
+   end
